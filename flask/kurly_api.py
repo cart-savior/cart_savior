4694c46919e4cb2187e68a23d4fd0c1e457bb0f2
Lana Chung <lana@Lanaui-MacBookPro.local>
2020-05-02 19:04:16 +0900

    update get_kurly_keyword

--- flask/kurly_api.py
+++ flask/kurly_api.py
@@ -1,5 +1,5 @@
 import requests
-item_name = '쌀'
+#item_name = '쌀'
 
 def get_kurly(item_name):
 	req = requests.get('https://api.kurly.com/v1/search?keyword="' + item_name + '"&sort_type=-1&page_limit=99&page_no=1&delivery_type=0&ver=1588412210623')
@@ -12,5 +12,3 @@ def get_kurly(item_name):
 		'link' : "https://www.kurly.com/shop/goods/goods_view.php?&goodsno=" + product['no']
 		} for product in products][:3]
 	return result
-
-print(get_kurly(item_name))
