8e017139d8533b930e16c23a027e69dccd2bf33d
Lana Chung <lana@Lanaui-MacBookPro.local>
2020-05-03 17:50:19 +0900

    Update market_api.py add hanaro add random

--- flask/market_api.py
+++ flask/market_api.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import random
 #item_name = '쌀'
 
 def get_kurly(item_name):
@@ -12,27 +13,47 @@ def get_kurly(item_name):
 		'price': product['price'],
 		'image' : product['thumbnail_image_url'],
 		'link' : "https://www.kurly.com/shop/goods/goods_view.php?&goodsno=" + product['no']
-		} for product in products][:3]
+		} for product in products][:2]
 	return result
 
 def get_ssg(item_name):
 	req = requests.get('http://www.ssg.com/search.ssg?target=all&query='+ item_name)
 	html = req.text
 	soup = BeautifulSoup(html, 'html.parser')
-	name = [name.text for name in soup.select('div > a > em.tx_ko')[:3]]
-	price = [price.text + "원" for price in soup.select('div > em')[:3]]
-	image = [ "http:"+ image.get('src') for image in soup.select('div > a > img.i1')[:3]]
-	link = ["ssg.com" + link.get('href') for link in soup.select('div.thmb > a')[:3]]
+	name = [name.text for name in soup.select('div > a > em.tx_ko')[:2]]
+	price = [price.text + "원" for price in soup.select('div > em')[:2]]
+	image = [ "http:"+ image.get('src') for image in soup.select('div > a > img.i1')[:2]]
+	link = ["ssg.com" + link.get('href') for link in soup.select('div.thmb > a')[:2]]
 	result = [{
 		'site' : "ssg",
 		'name' : name[i],
 		'price' : price[i],
 		'image' : image[i],
 		'link' : link[i]
-		} for i in range(3)]
+		} for i in range(2)]
 	return result
 
-def add_kurly_ssg(item_name):
+def get_hanaro(item_name):
+	req = requests.get('http://www.nonghyupmall.com/BC1F010M/srchTotalList.nh?searchTerm_main=' + item_name + '%40undefined&CHAN_C=1102&chanC=1102')
+	html = req.text
+	soup = BeautifulSoup(html, 'html.parser')
+	name = [name.text for name in soup.select('div > a > p')][:2]
+	price = [price.text[62:-52] for price in soup.select('div > p')][2:4]
+	image = ['www.nonghyupmall.com/' + image.get('src') for image in soup.select('div.product-thumb > img')][:2]
+	link = ['http://www.nonghyupmall.com/BC14010R/viewDetail.nh?wrsC=' + code.get('data-wrs-c') +'&basketCnt=0' for code in soup.select('div.product-info-area > a')][:2]
+	result = [{
+	    'site' : "hanaro",
+	    'name' : name[i],
+	    'price' : price[i],
+	    'image' : image[i],
+	    'link' : link[i]
+	    } for i in range(2)]
+	return result
+
+def add_all_market(item_name):
 	kurly = get_kurly(item_name)
 	ssg = get_ssg(item_name)
-	return kurly + ssg
+	hanaro = get_hanaro(item_name)
+	result = kurly + ssg + hanaro
+	random.shuffle(result)
+	return result
